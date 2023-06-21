b22f146a50dfc06bc3bb93a9a7d2507494cd1acb
Sylwestrzak1109 <130354021+Sylwestrzak1109@users.noreply.github.com>
2023-06-21 18:19:29 +0200

    Update app.py

--- Protokol/app.py
+++ Protokol/app.py
@@ -17,30 +17,31 @@ db_connection = mysql.connector.connect(
 )
 
 class MyForm(FlaskForm):
-    oswietlenie=SelectField('Oświetlenie', choices=[('naturalne'), ('sztuczne'), ('dobre'), ('złe')])
-    pogoda=SelectField('Warunki pogodowe', choices=[('słonecznie'), ('zachmurzenie'), ('deszcz'), ('śnieg'),('mgła')])
     pora = SelectField('Pora dnia', choices=[('dzienna'), ('nocna'), ('zmierzch'), ('świt')])
-
+    rodzaj=SelectField('Rodzaj przestępstwa', choices=[('Kradzież'), ('Napad'), ('Zabójstwo'), ('Przemoc Fizyczna')])
+    miejsce=SelectField('Miejsce popełnienia przestępstwa', choices=[('na ulicy'), ('dom/mieszkanie'), ('miejsce publiczne')])
+    bron=SelectField('czy sprawca był uzbrojony',choices=[('tak'),('nie')])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = MyForm()
     if form.validate_on_submit():
-        oswietlenie = form.oswietlenie.data
-        pogoda = form.pogoda.data
+        rodzaj = form.rodzaj.data
+        miejsce = form.miejsce.data
         pora = form.pora.data
+        bron=form.bron.data
 
         cursor = db_connection.cursor()
-        query = "INSERT INTO dane (oswietlenie, pogoda, pora) VALUES (%s, %s, %s)"
-        values = (oswietlenie, pogoda, pora)
+        query = "INSERT INTO dane (rodzaj, miejsce, pora,bron) VALUES (%s, %s, %s, %s)"
+        values = (rodzaj, miejsce, pora, bron)
         cursor.execute(query, values)
         db_connection.commit()
         cursor.close()
 
-        print(f'Oświetlenie: {oswietlenie}')
-        print(f'Pogoda: {pogoda}')
+        print(f'rodzaj: {rodzaj}')
+        print(f'miejsce: {miejsce}')
         print(f'Pora dnia: {pora}')
-
+        print(f'Uzbrojenie: {bron}')
         return 'Dane zostały przesłane poprawnie!'
     return render_template('form.html', form=form)
 
